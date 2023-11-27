806c96e1bbc30630bec9ee0af98f0329c1df882c
pnovich <pppp_84@ukr.net>
2023-11-27 23:46:51 +0200

    initial commit

---
+++ src/main/java/com/example/messageconsumer/MessageconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.messageconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageconsumerApplication.class, args);
+	}
+
+}
